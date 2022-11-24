74000a86f471283c10f4b5d8427dfee6d5d452ec
Andrew Cook <acook999@gmail.com>
2022-11-23 20:35:59 -0600

    evernote working with libraries. set up pseudocode in main app file

--- evernote_read.py
+++ evernote_read.py
@@ -6,7 +6,9 @@ from evernote.api.client import EvernoteClient
 
 
 def main():
-    client = EvernoteClient(token=PASS.personal_token, sandbox=False)
+    #find the token by going to evernote web and finding it in the cookies. "auth" cookie
+    evernote_token = PASS.evernote_token
+    client = EvernoteClient(token=evernote_token, sandbox=False)
     userStore = client.get_user_store()
     noteStore = client.get_note_store()
     notebooks = noteStore.listNotebooks()
@@ -26,13 +28,13 @@ def main():
     resultSpec.includeTagGuids = True
     resultSpec.includeLargestResourceMime = True
     resultSpec.includeLargestResourceSize = True
-    notes = noteStore.findNotesMetadata(PASS.personal_token, filter, 0, 100, resultSpec) 
-    
+
+    notes = noteStore.findNotesMetadata(evernote_token, filter, 0, 100, resultSpec)     
     i = 0
     for note in notes.notes:
         if i < 10:
             print(note.guid)
-            content = noteStore.getNoteContent(PASS.personal_token, note.guid)
+            content = noteStore.getNoteContent(evernote_token, note.guid)
             print(content)
             i = i + 1
 
